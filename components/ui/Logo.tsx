c764e3680514437c4095530f5716c070f0864930
Tomilola-ng <tee.o2809@gmail.com>
2024-10-19 10:19:00 +0100

    Setup

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <Link href="/">
+        Rodela <span className='text-primary'>Cleaners</span>
+    </Link>
+  )
+}
